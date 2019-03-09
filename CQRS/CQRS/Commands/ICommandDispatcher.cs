a4e6584c90ec22790e3007df97c16692a40b8130
Brandon Pen <brandon.pen@outlook.com>
2019-03-09 18:48:53 -0500

    - Initial commit - Added implementations of ICommandHandler, both with return types and with a return type - Added asynchronous implementations - Added implementation of ICommandDispatcher

---
+++ CQRS/CQRS/Commands/ICommandDispatcher.cs
@@ -0,0 +1,38 @@
+﻿namespace CQRS.Commands
+{
+    /// <summary>
+    /// The ICommandDispatcher interface is responsible for locating and calling the Handle method of a the appropriate <see cref="ICommandHandler{TCommand}"/>, based
+    /// on the type of the specified <see cref="TCommand"/>.
+    /// </summary>
+    /// <typeparam name="TCommand" />
+    public interface ICommandDispatcher<TCommand>
+    {
+        /// <summary>
+        /// The Handle method should invoke the Handle method of the appropriate <see cref="ICommandHandler{TCommand}"/> method.
+        /// </summary>
+        /// <param name="command">
+        /// The command.
+        /// </param>
+        void Handle(TCommand command);
+    }
+
+    /// <summary>
+    /// The ICommandDispatcher interface is responsible for locating and calling the Handle method of a the appropriate <see cref="ICommandHandler{TCommand, TResult}"/>, based
+    /// on the type of the specified <see cref="TCommand"/>.  
+    /// </summary>
+    /// <typeparam name="TCommand" />
+    /// <typeparam name="TResult" />
+    public interface ICommandDispatcher<TCommand, TResult>
+    {
+        /// <summary>
+        /// The Handle method should invoke the Handle method of the appropriate <see cref="ICommandHandler{TCommand}"/> method.
+        /// </summary>
+        /// <param name="command">
+        /// The command.
+        /// </param>
+        /// <returns>
+        /// Returns a result of type <see cref="TResult"/>.
+        /// </returns>
+        TResult Handle(TCommand command);
+    }
+}
---
+++ CQRS/CQRS/Commands/ICommandDispatcherAsync.cs
@@ -0,0 +1,24 @@
+﻿namespace CQRS.Commands
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The ICommandDispatcherAsync interface is responsible for locating and calling the Handle method of a the appropriate <see cref="ICommandHandlerAsync{TCommand, TResult}"/>, based
+    /// on the type of the specified <see cref="TCommand"/> and <see cref="TResult"/>.  
+    /// </summary>
+    /// <typeparam name="TCommand" />
+    /// <typeparam name="TResult" />
+    public interface ICommandDispatcherAsync<TCommand, TResult>
+    {
+        /// <summary>
+        /// The HandleAsync method should invoke the HandleAsync method of the appropriate <see cref="ICommandHandlerAsync{TCommand, TResult}"/> method.
+        /// </summary>
+        /// <param name="command">
+        /// The command.
+        /// </param>
+        /// <returns>
+        /// Returns a result of type <see cref="Task{TResult}"/>.
+        /// </returns>
+        Task<TResult> HandleAsync(TCommand command);
+    }
+}
