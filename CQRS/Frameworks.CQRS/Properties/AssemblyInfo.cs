65bb2f82db11a07b6b1b93bf1e8cbe4b3d3b7626
Brandon Pen <brandon.pen@outlook.com>
2019-03-09 20:31:13 -0500

    - Modified assembly details to represent project's intended purpose - Added .nuspec file

--- CQRS/Frameworks.CQRS/Properties/AssemblyInfo.cs
+++ CQRS/Frameworks.CQRS/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Frameworks.CQRS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A basic implementation of the CQRS (Command/Query Responsibility Segregation) design pattern.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("iAm9001")]
 [assembly: AssemblyProduct("Frameworks.CQRS")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
